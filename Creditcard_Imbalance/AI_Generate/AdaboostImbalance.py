ae5fea8b7e6914e690df97e3704e02eb51d056e0
honjeng akasimina <1074368873@qq.com>
2023-04-11 17:13:01 +0800

    AI_Generate's codes are generated by GPT-4 .These codes is about imbalanced dataset

---
+++ Creditcard_Imbalance/AI_Generate/AdaboostImbalance.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from imblearn.over_sampling import SMOTE
+from sklearn.decomposition import PCA
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+# 加载数据
+data = pd.read_csv('../creditcard.csv')
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+# 划分训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 过采样
+smote = SMOTE(random_state=42)
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+# PCA降维
+pca = PCA(n_components=10)  # 选择10个主成分
+X_train_pca = pca.fit_transform(X_train_resampled)
+X_test_pca = pca.transform(X_test)
+
+# AdaBoost训练
+adb = AdaBoostClassifier(random_state=42)
+adb.fit(X_train_pca, y_train_resampled)
+
+# 预测和评估
+y_pred = adb.predict(X_test_pca)
+print(classification_report(y_test, y_pred))
